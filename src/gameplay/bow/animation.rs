d5279c2e7551f0ba13a4f04ce2bb81e486a37663
dsgallups <dsgallups@protonmail.com>
2025-06-01 11:06:46 -0400

    scaffold animation

---
+++ src/gameplay/bow/animation.rs
@@ -0,0 +1,71 @@
+use std::iter;
+
+use bevy::{animation::RepeatAnimation, prelude::*, scene::SceneInstanceReady};
+
+use crate::gameplay::GameSet;
+
+use super::{Bow, BowAssets, pull::PullStrength};
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_systems(Update, update_bow_pull.in_set(GameSet::Update));
+}
+
+#[derive(Component)]
+struct BowAnimation {
+    player: Entity,
+    index: AnimationNodeIndex,
+}
+// an observer for the spawned bow. not global.
+pub(super) fn setup_animations(
+    trigger: Trigger<SceneInstanceReady>,
+    mut commands: Commands,
+    assets: Res<BowAssets>,
+    children: Query<&Children>,
+    animation_players: Query<(), With<AnimationPlayer>>,
+    scene_root: Query<Entity, With<Bow>>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
+) {
+    let target = trigger.target();
+    let Some(animation_player_entity) = children
+        .iter_descendants(target)
+        .find(|child| animation_players.get(*child).is_ok())
+    else {
+        error!("No animation player for scene!");
+        return;
+    };
+
+    let bow = scene_root.get(trigger.target()).unwrap();
+
+    let (animation_graph, node) = AnimationGraph::from_clip(assets.pull_string.clone());
+
+    let graph_handle = graphs.add(animation_graph);
+
+    commands
+        .entity(animation_player_entity)
+        .insert((AnimationGraphHandle(graph_handle)));
+
+    commands.entity(bow).insert(BowAnimation {
+        player: animation_player_entity,
+        index: node,
+    });
+}
+
+fn update_bow_pull(
+    bow_pull: Query<(&BowAnimation, &PullStrength)>,
+    mut animations: Query<&mut AnimationPlayer>,
+) {
+    let Ok((anim_props, pull_strength)) = bow_pull.single() else {
+        return;
+    };
+    let mut anim_player = animations.get_mut(anim_props.player).unwrap();
+
+    if !anim_player.is_playing_animation(anim_props.index) {
+        let pull_animation = anim_player.play(anim_props.index);
+        pull_animation
+            .set_repeat(RepeatAnimation::Forever)
+            .set_speed(0.);
+    }
+
+    let pull_animation = anim_player.animation_mut(anim_props.index).unwrap();
+    pull_animation.seek_to(pull_strength.0);
+}
--- src/gameplay/bow/mod.rs
+++ src/gameplay/bow/mod.rs
@@ -1,47 +1,60 @@
 use bevy::prelude::*;
 use bevy_gltf_animation::prelude::*;
+use pull::PullStrength;
 
 use crate::asset_tracking::LoadResource;
 
 use super::{GameLoadState, GameSet, cursor::CursorPosition};
 
+mod animation;
+mod pull;
+
 pub(super) fn plugin(app: &mut App) {
     app.register_type::<BowAssets>()
         .load_resource::<BowAssets>();
+
+    app.add_plugins((pull::plugin, animation::plugin));
+
     app.add_systems(OnEnter(GameLoadState::Loaded), spawn_bow)
         .add_systems(Update, move_bow.in_set(GameSet::Update));
 }
 #[derive(Resource, Asset, Reflect, Clone)]
 struct BowAssets {
     #[dependency]
-    model: Handle<Gltf>,
+    scene: Handle<Scene>,
+    #[dependency]
+    pull_string: Handle<AnimationClip>,
 }
 impl FromWorld for BowAssets {
     fn from_world(world: &mut World) -> Self {
         let assets = world.resource::<AssetServer>();
         Self {
-            model: assets.load("models/Bow.glb"),
+            scene: assets.load("models/Bow.glb#Scene0"),
+            pull_string: assets.load("models/Bow.glb#Animation0"),
         }
     }
 }
 
 #[derive(Component)]
+#[require(PullStrength)]
 pub struct Bow;
 
 fn spawn_bow(mut commands: Commands, assets: Res<BowAssets>) {
     info!("Spawning bow");
-    commands.spawn((
-        Bow,
-        GltfSceneRoot::new(assets.model.clone()),
-        children![(
-            DirectionalLight {
-                illuminance: light_consts::lux::CLEAR_SUNRISE,
-                shadows_enabled: true,
-                ..default()
-            },
-            Transform::from_xyz(0., 0., 5.).looking_at(Vec3::ZERO, Vec3::Y),
-        )],
-    ));
+    commands
+        .spawn((
+            Bow,
+            SceneRoot(assets.scene.clone()),
+            children![(
+                DirectionalLight {
+                    illuminance: light_consts::lux::CLEAR_SUNRISE,
+                    shadows_enabled: true,
+                    ..default()
+                },
+                Transform::from_xyz(0., 0., 5.).looking_at(Vec3::ZERO, Vec3::Y),
+            )],
+        ))
+        .observe(animation::setup_animations);
 }
 fn move_bow(cursor: Res<CursorPosition>, mut bow: Query<&mut Transform, With<Bow>>) {
     let Ok(mut bow) = bow.single_mut() else {
