dd8e3dd264267f444d178f34894a93ec6b806726
dsgallups <dsgallups@protonmail.com>
2025-06-02 07:26:06 -0400

    get title screen camera position

--- src/camera/world.rs
+++ src/camera/world.rs
@@ -6,15 +6,12 @@ use bevy::{
 };
 
 use crate::{
-    Screen,
     camera::{CameraOrder, RenderLayer},
-    gameplay::GAMEPLAY_CAMERA_OFFSET,
     world::backdrop::{BACKDROP_OFFSET, BLOCK_LEN},
 };
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(Startup, spawn_world_camera)
-        .add_systems(Update, set_camera_position.run_if(in_state(Screen::Title)));
+    app.add_systems(Startup, spawn_world_camera);
 }
 
 #[derive(Component, Reflect)]
@@ -22,7 +19,20 @@ pub(super) fn plugin(app: &mut App) {
 pub struct WorldCamera;
 
 fn spawn_world_camera(mut commands: Commands) {
-    commands.spawn((
+    // this is the title screen position
+    let pos = Vec3::new(
+        BLOCK_LEN * 7. + 4.,
+        BLOCK_LEN * 3. + 4.,
+        1. - BACKDROP_OFFSET,
+    );
+
+    let look_at = Vec3::new(
+        BLOCK_LEN * 6. + 3.,
+        BLOCK_LEN * 4. + 3.,
+        -3. - BACKDROP_OFFSET,
+    );
+
+    *cam = commands.spawn((
         Name::new("World Camera"),
         Camera3d::default(),
         WorldCamera,
@@ -33,7 +43,7 @@ fn spawn_world_camera(mut commands: Commands) {
             ..default()
         },
         Tonemapping::TonyMcMapface, // 2. Using a tonemapper that desaturates to white is recommended
-        Transform::from_xyz(0., 0., GAMEPLAY_CAMERA_OFFSET).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_translation(pos).looking_at(look_at, Vec3::Z),
         MeshPickingCamera,
         Projection::from(PerspectiveProjection {
             fov: 45.0_f32.to_radians(),
@@ -53,30 +63,4 @@ fn spawn_world_camera(mut commands: Commands) {
     //     },
     //     Transform::from_xyz(0.0, 16.0, 0.0),
     // ));
-}
-
-#[cfg_attr(feature = "hot", bevy_simple_subsecond_system::prelude::hot)]
-fn set_camera_position(mut cam: Query<&mut Transform, With<WorldCamera>>) {
-    use crate::world::backdrop::BLOCK_LEN;
-
-    let mut cam = cam.single_mut().unwrap();
-
-    let trans_x = 3.;
-
-    let trans_y = 3.;
-
-    let trans_z = -1.;
-
-    let pos = Vec3::new(
-        1. + BLOCK_LEN * 7. + trans_x,
-        1. + BLOCK_LEN * 3. + trans_y,
-        2. + trans_z - BACKDROP_OFFSET,
-    );
-    let look_at = Vec3::new(
-        BLOCK_LEN * 6. + trans_x,
-        BLOCK_LEN * 4. + trans_y,
-        -2. + trans_z - BACKDROP_OFFSET,
-    );
-
-    *cam = Transform::from_translation(pos).looking_at(look_at, Vec3::Z);
 }
