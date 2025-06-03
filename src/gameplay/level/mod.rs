f959b328112a87074928a45be7d5f95995300f59
dsgallups <dsgallups@protonmail.com>
2025-06-03 07:29:53 -0400

    init debug level

--- src/gameplay/level/mod.rs
+++ src/gameplay/level/mod.rs
@@ -2,6 +2,9 @@ use bevy::prelude::*;
 
 use crate::Screen;
 
+#[cfg(feature = "dev")]
+mod debug;
+
 mod infinite;
 mod one;
 mod three;
@@ -12,7 +15,10 @@ mod two;
 #[source(Screen = Screen::Gameplay)]
 #[states(scoped_entities)]
 pub enum LevelState {
+    #[cfg(feature = "dev")]
     #[default]
+    Debug,
+    #[cfg_attr(not(feature = "dev"), default)]
     One,
     Two,
     Three,
@@ -23,11 +29,16 @@ pub(super) fn plugin(app: &mut App) {
     app.init_resource::<Level>().add_sub_state::<LevelState>();
 
     app.add_plugins((one::plugin, two::plugin, three::plugin, infinite::plugin));
+
+    #[cfg(feature = "dev")]
+    app.add_plugins(debug::plugin);
 }
 
 #[derive(Resource)]
+#[cfg_attr(feature = "dev", derive(Default))]
 pub struct Level(pub usize);
 
+#[cfg(not(feature = "dev"))]
 impl Default for Level {
     fn default() -> Self {
         Level(1)
