fdbecd3baa77086c163c974aa9c31e9ca2bd4fe2
dsgallups <dsgallups@protonmail.com>
2025-06-04 06:27:50 -0400

    init ui plugin

---
+++ src/gameplay/ui/mod.rs
@@ -0,0 +1,54 @@
+use bevy::prelude::{Val::*, *};
+
+use crate::Screen;
+
+pub(super) fn plugin(app: &mut App) {
+    app.add_systems(OnEnter(Screen::Gameplay), setup);
+
+    //todo
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        Name::new("UI Root"),
+        StateScoped(Screen::Gameplay),
+        UiRoot,
+        Node {
+            width: Percent(100.),
+            height: Percent(100.),
+            flex_direction: FlexDirection::Column,
+            ..default()
+        },
+        children![header(), content(), footer()],
+        Pickable::IGNORE,
+    ));
+}
+
+#[derive(Component)]
+pub struct UiRoot;
+
+#[derive(Component)]
+pub struct Header;
+
+#[derive(Component)]
+pub struct Content;
+
+#[derive(Component)]
+pub struct Footer;
+
+fn header() -> impl Bundle {
+    (Node::default(), Pickable::IGNORE, Header)
+}
+fn content() -> impl Bundle {
+    (
+        Node {
+            flex_grow: 1.,
+            ..default()
+        },
+        Pickable::IGNORE,
+        Content,
+    )
+}
+fn footer() -> impl Bundle {
+    (Node::default(), Pickable::IGNORE, Footer)
+}
