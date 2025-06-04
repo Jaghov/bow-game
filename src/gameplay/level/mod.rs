c694bc49b2ff9bac40a64c3c380eb2b19c5ffdfd
dsgallups <dsgallups@protonmail.com>
2025-06-04 09:15:24 -0400

    move over old levels

--- src/gameplay/level/mod.rs
+++ src/gameplay/level/mod.rs
@@ -1,46 +1,14 @@
 use bevy::prelude::*;
 
-use crate::Screen;
+pub(super) fn plugin(app: &mut App) {
+    //todo
+}
 
-#[cfg(feature = "dev")]
-mod debug;
-
-mod infinite;
-mod one;
-mod three;
-mod two;
-
-/// I'm so sorry levels start at one
-#[derive(SubStates, Clone, PartialEq, Eq, Hash, Debug, Default, Reflect)]
-#[source(Screen = Screen::Gameplay)]
+#[derive(States, Debug, Hash, PartialEq, Eq, Clone, Copy, Default, Reflect)]
 #[states(scoped_entities)]
 pub enum LevelState {
-    #[cfg(feature = "dev")]
     #[default]
-    Debug,
-    #[cfg_attr(not(feature = "dev"), default)]
-    One,
-    Two,
-    Three,
-    Infinite,
-}
-
-pub(super) fn plugin(app: &mut App) {
-    app.init_resource::<Level>().add_sub_state::<LevelState>();
-
-    app.add_plugins((one::plugin, two::plugin, three::plugin, infinite::plugin));
-
-    #[cfg(feature = "dev")]
-    app.add_plugins(debug::plugin);
-}
-
-#[derive(Resource)]
-#[cfg_attr(feature = "dev", derive(Default))]
-pub struct Level(pub usize);
-
-#[cfg(not(feature = "dev"))]
-impl Default for Level {
-    fn default() -> Self {
-        Level(1)
-    }
+    NewLevel,
+    Playing,
+    NextLevel,
 }
