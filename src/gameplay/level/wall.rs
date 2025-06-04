44d05e63845afa555bec8de100ddf0f3a86da401
dsgallups <dsgallups@protonmail.com>
2025-06-04 14:36:53 -0400

    fix: remove near/far code from wall builder

--- src/gameplay/level/wall.rs
+++ src/gameplay/level/wall.rs
@@ -11,15 +11,9 @@ pub struct WallBuilder {
 impl WallBuilder {
     // note that these values are not multiplied by `BLOCK_LEN`.
     pub fn horizontal(y: f32, start_x: f32, end_x: f32) -> Self {
-        let (near_x, far_x) = if start_x < end_x {
-            (start_x, end_x)
-        } else {
-            (end_x, start_x)
-        };
+        let length = (end_x - start_x).abs();
 
-        let length = far_x - near_x;
-
-        let middle_x = (far_x + near_x) * 0.5;
+        let middle_x = (start_x + end_x) * 0.5;
 
         let mesh = Cuboid::new(length, BLOCK_LEN, BLOCK_LEN);
 
@@ -33,15 +27,9 @@ impl WallBuilder {
         }
     }
     pub fn vertical(x: f32, start_y: f32, end_y: f32) -> Self {
-        let (near_y, far_y) = if start_y < end_y {
-            (start_y, end_y)
-        } else {
-            (end_y, start_y)
-        };
+        let length = (end_y - start_y).abs();
 
-        let length = far_y - near_y;
-
-        let middle_y = (far_y + near_y) * 0.5;
+        let middle_y = (start_y + end_y) * 0.5;
 
         let mesh = Cuboid::new(BLOCK_LEN, length, BLOCK_LEN);
 
