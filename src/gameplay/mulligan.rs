9175b7824d2288444491f5d4239d5d9980f30880
dsgallups <dsgallups@protonmail.com>
2025-06-08 12:21:59 -0400

    add mulligan systems

--- src/gameplay/mulligan.rs
+++ src/gameplay/mulligan.rs
@@ -1,15 +1,36 @@
-use bevy::prelude::*;
+use bevy::{platform::collections::HashMap, prelude::*};
+
+use crate::{
+    Screen,
+    gameplay::level::{Level, LevelState},
+};
 
 pub(super) fn plugin(app: &mut App) {
-    app.init_resource::<MulliganPossible>();
+    app.init_resource::<Mulligan>()
+        .add_systems(OnEnter(LevelState::NewLevel), update_mulligans)
+        .add_systems(OnEnter(Screen::Gameplay), reset_mulligans);
     //todod
 }
 
-#[derive(Resource)]
-pub struct MulliganPossible(bool);
+#[derive(Resource, Default)]
+pub struct Mulligan {
+    mulligans_used: HashMap<usize, u8>,
+}
 
-impl Default for MulliganPossible {
-    fn default() -> Self {
-        Self(true)
+impl Mulligan {
+    pub fn can_mulligan(&mut self, level: usize) -> bool {
+        match self.mulligans_used.get(&level) {
+            Some(used) => *used < 2,
+            None => true,
+        }
     }
+}
+
+fn reset_mulligans(mut mulligans: ResMut<Mulligan>) {
+    mulligans.mulligans_used.clear();
+}
+
+fn update_mulligans(level: Res<Level>, mut mulligans: ResMut<Mulligan>) {
+    let used = mulligans.mulligans_used.entry(level.0).or_default();
+    *used += 1;
 }
