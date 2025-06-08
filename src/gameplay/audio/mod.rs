60d713f159e9c54796ea7b5be760a51bd7d83bbb
Joseph <Jaghoghov@gmail.com>
2025-06-08 14:33:55 +0100

    feat: added gameplay theme

--- src/gameplay/audio/mod.rs
+++ src/gameplay/audio/mod.rs
@@ -5,12 +5,15 @@ use crate::{Screen, asset_tracking::LoadResource};
 pub fn plugin(app: &mut App) {
     app.load_resource::<MusicTracks>()
         .add_systems(OnEnter(Screen::Title), play_menu_theme)
+        .add_systems(OnEnter(Screen::Transition), play_gameplay_theme)
         .add_systems(Update, (pause, mute, volume));
 }
 #[derive(Asset, Resource, Reflect, Clone)]
 struct MusicTracks {
     #[dependency]
     menu: Handle<AudioSource>,
+    #[dependency]
+    game: Handle<AudioSource>,
 }
 
 impl FromWorld for MusicTracks {
@@ -18,12 +21,16 @@ impl FromWorld for MusicTracks {
         let asset_server = world.resource::<AssetServer>();
         Self {
             menu: asset_server.load("audio/music/Menu Theme - V2 - BevyJam6.flac"),
+            game: asset_server.load("audio/music/InGame Music - V2 - BevyJam6.flac"),
         }
     }
 }
+#[derive(Component)]
+struct Music;
 
 fn play_menu_theme(mut commands: Commands, tracks: Res<MusicTracks>) {
     commands.spawn((
+        Music,
         AudioPlayer(tracks.menu.clone()),
         PlaybackSettings {
             mode: bevy::audio::PlaybackMode::Loop,
@@ -32,7 +39,35 @@ fn play_menu_theme(mut commands: Commands, tracks: Res<MusicTracks>) {
     ));
 }
 
-fn pause(keyboard_input: Res<ButtonInput<KeyCode>>, music_controller: Query<&AudioSink>) {
+fn play_gameplay_theme(
+    mut commands: Commands,
+    tracks: Res<MusicTracks>,
+    player: Query<(Entity), With<Music>>,
+) {
+    info!("Playing gameplay theme");
+    let bgm = (
+        AudioPlayer(tracks.game.clone()),
+        PlaybackSettings {
+            mode: bevy::audio::PlaybackMode::Loop,
+            ..Default::default()
+        },
+    );
+    match player.single() {
+        Ok((entity)) => {
+            commands.entity(entity).remove::<AudioSink>();
+            commands.entity(entity).insert(bgm);
+        }
+        Err(_) => {
+            commands.spawn((bgm, Music));
+        }
+    };
+}
+
+// Toggles between menu and bgm
+fn pause(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    music_controller: Query<&AudioSink, With<Music>>,
+) {
     let Ok(sink) = music_controller.single() else {
         return;
     };
@@ -42,7 +77,10 @@ fn pause(keyboard_input: Res<ButtonInput<KeyCode>>, music_controller: Query<&Aud
     }
 }
 
-fn mute(keyboard_input: Res<ButtonInput<KeyCode>>, mut music_controller: Query<&mut AudioSink>) {
+fn mute(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut music_controller: Query<&mut AudioSink, With<Music>>,
+) {
     let Ok(mut sink) = music_controller.single_mut() else {
         return;
     };
@@ -52,7 +90,10 @@ fn mute(keyboard_input: Res<ButtonInput<KeyCode>>, mut music_controller: Query<&
     }
 }
 
-fn volume(keyboard_input: Res<ButtonInput<KeyCode>>, mut music_controller: Query<&mut AudioSink>) {
+fn volume(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut music_controller: Query<&mut AudioSink, With<Music>>,
+) {
     let Ok(mut sink) = music_controller.single_mut() else {
         return;
     };
